6c826eebde94d2e367b1bfa69aa0425d8613bdb0
Sueyuki <2574397962@qq.com>
2024-04-05 16:03:31 +0800

    完成了筛选，搜索功能的本地部分

--- backend/routes/travelogRoutes.js
+++ backend/routes/travelogRoutes.js
@@ -2,7 +2,7 @@
  * @Author: Sueyuki 2574397962@qq.com
  * @Date: 2024-04-02 19:17:09
  * @LastEditors: Sueyuki 2574397962@qq.com
- * @LastEditTime: 2024-04-03 20:52:26
+ * @LastEditTime: 2024-04-05 14:45:19
  * @FilePath: \backend\routes\travelogRoutes.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -35,18 +35,12 @@ router.get("/mytravelogs", async (req, res) => {
 //游记上传 待优化
 router.post("/travelogs", imgUpload.array("image"), async (req, res, next) => {
   const userId = req.userId
-<<<<<<< HEAD
   console.log("req.body", req.body)
   const { title, content, tripWay, tripNum, tripDate, tripBudget, city, rate } = req.body.editingData
   console.log("editingData", title, content, tripWay, tripNum, tripDate, tripBudget, city, rate)
   // 访问文件列表中的每个文件对象
   // console.log(req.body.imgInfo)
   const imgInfo = req.body.imgInfo
-=======
-
-  const { title, content, tags } = req.body
-  const imgInfo = JSON.parse(req.body.imgInfo)
->>>>>>> feature/admin
   const files = req.files
   const imgInfoValue = JSON.parse(imgInfo.value)
   const orderedImgName = imgInfoValue.map(originalname => {
--- frontend/src/Context/HomeContext.js
+++ frontend/src/Context/HomeContext.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: Sueyuki 2574397962@qq.com
+ * @Date: 2024-04-04 20:33:27
+ * @LastEditors: Sueyuki 2574397962@qq.com
+ * @LastEditTime: 2024-04-05 15:32:14
+ * @FilePath: \frontend\src\Context\HomeContext.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 // HomeContext.js
 
 import React, { createContext, useState } from 'react';
@@ -5,8 +13,9 @@ import React, { createContext, useState } from 'react';
 export const HomeContext = createContext();
 
 export const HomeContextProvider = ({ children }) => {
+  const [searchTerm, setSearchTerm] = useState('');
   const [sorter, setSorter] = useState('0');
-  const [city, setCity] = useState();
+  const [city, setCity] = useState([]);
   const [selectedFilters, setSelectedFilters] = useState({
     tripWay: null,
     tripNum: null,
@@ -16,7 +25,7 @@ export const HomeContextProvider = ({ children }) => {
   });
 
   return (
-    <HomeContext.Provider value={{ sorter, setSorter, city, setCity, selectedFilters, setSelectedFilters }}>
+    <HomeContext.Provider value={{ sorter, setSorter, city, setCity, selectedFilters, setSelectedFilters,searchTerm, setSearchTerm }}>
       {children}
     </HomeContext.Provider>
   );
