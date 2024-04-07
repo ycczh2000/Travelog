c45e0153dcf330cbd115d9e333b4323d05a4d5c6
Sueyuki <2574397962@qq.com>
2024-04-08 01:46:33 +0800

    antd部分组件存在问题，换用其他实现

--- frontend/src/pages/MyTravelog/MyTravelog.jsx
+++ frontend/src/pages/MyTravelog/MyTravelog.jsx
@@ -6,7 +6,11 @@ import { UpOutline, CloseCircleFill, StarFill } from "antd-mobile-icons"
 import styles from "./MyTravelog.module.scss"
 import { UserSpaceContentProvider } from "./UserSpaceContent"
 import { UserContext } from "../../Context/UserContext"
-import { card, Popover } from "antd-mobile"
+import { Popup, Button } from 'antd-mobile'
+import { UploadOutlined } from '@ant-design/icons';
+import { Upload } from 'antd';
+import AvatarEditor from "react-avatar-editor";
+
 import './MyTravelog.css';
 
 const data = [
@@ -60,6 +64,41 @@ export default function MyTravelog() {
   console.log(userName, UID)
   const [myTravelogList, setMyTravelogList] = useState(data)
   const [totop, setTotop] = useState(true)
+  const [visible, setVisible] = useState(false)//标志上传头像的弹出组件是否弹出
+  const [avatarFile, setAvatarFile] = useState([
+    {
+      uid: '-1',
+      name: 'image.png',
+      status: 'done',
+      url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+    },
+  ]);
+  const onChange = ({ avatarFile: newAvatarFile }) => {
+    console.log('onChange', newAvatarFile)
+    setAvatarFile(newAvatarFile);
+  };
+  const onPreview = async (file) => {
+    console.log('onPreview', file)
+    let src = file.url;
+    if (!src) {
+      src = await new Promise((resolve) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file.originFileObj);
+        reader.onload = () => resolve(reader.result);
+      });
+    }
+    const image = new Image();
+    image.src = src;
+    const imgWindow = window.open(src);
+    imgWindow?.document.write(image.outerHTML);
+  };
+  const changeAvatarComponent = (
+    <div>
+      <Button block size='large' style={{ margin: '10px' }} onClick={() =>{setVisible(false)}}>
+        取消
+      </Button>
+    </div>
+  )
   // const [username, setUsername] = useState("MOMO")
   // const [uid, setUid] = useState("1145141919810")
   const [avatarUrl, setAvatarUrl] = useState("https://img1.baidu.com/it/u=1389873612,485301600&fm=253&app=120&size=w931&n=0&f=JPEG&fmt=auto?sec=1712595600&t=76261ab2a1585815f46c7b306c6f66e3")
@@ -69,19 +108,30 @@ export default function MyTravelog() {
         <div className="background-image"></div>
         <MyTravelogHeader />
         <div className="user-space">
-
           <div className="content">
-            <div className="avatar">
+            <div className="avatar" onClick={() => setVisible(true)}>
+              <Popup
+                visible={visible}
+                onMaskClick={() => {
+                  setVisible(false)
+                }}
+                bodyStyle={{
+                  borderTopLeftRadius: '8px',
+                  borderTopRightRadius: '8px',
+                  minHeight: '40vh',
+                }}
+              >{changeAvatarComponent}
+              </Popup>
               <img src={avatarUrl} alt="User Avatar" />
             </div>
-
             <div className="user-details">
               <div className="username">{userName ? userName : "MOMO"}</div>
               <div className="uid">UID: {UID ? UID : 1145141919810}</div>
+              <div className="uid">点击头像可以更换</div>
             </div>
           </div>
         </div>
-        
+
         <MyTravelogFilter />
         <MyTravelogList myTravelogList={myTravelogList} />
         {totop ? (
