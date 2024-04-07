4e51448c402bb73e73d5d5600cdc4a1da1d84258
Sueyuki <2574397962@qq.com>
2024-04-07 22:14:55 +0800

    todo:头像上传，按用户名搜索，关注/点赞（本地）

--- frontend/src/pages/MyTravelog/MyTravelog.jsx
+++ frontend/src/pages/MyTravelog/MyTravelog.jsx
@@ -4,10 +4,11 @@ import MyTravelogHeader from "./components/MyTravelogHeader/MyTravelogHeader"
 import MyTravelogFilter from "./components/MyTravelogFilter/MyTravelogFilter"
 import { UpOutline, CloseCircleFill, StarFill } from "antd-mobile-icons"
 import styles from "./MyTravelog.module.scss"
-import {UserSpaceContentProvider} from "./UserSpaceContent"
+import { UserSpaceContentProvider } from "./UserSpaceContent"
 import { UserContext } from "../../Context/UserContext"
-import { card } from "antd-mobile"
+import { card, Popover } from "antd-mobile"
 import './MyTravelog.css';
+
 const data = [
   {
     id: 1,
@@ -64,37 +65,40 @@ export default function MyTravelog() {
   const [avatarUrl, setAvatarUrl] = useState("https://img1.baidu.com/it/u=1389873612,485301600&fm=253&app=120&size=w931&n=0&f=JPEG&fmt=auto?sec=1712595600&t=76261ab2a1585815f46c7b306c6f66e3")
   return (
     <>
-    <UserSpaceContentProvider>
-     <div className="background-image"></div>
-      <MyTravelogHeader />
-      <div className="user-space">
-     
-      <div className="content">
-        <div className="avatar">
-          <img src={avatarUrl} alt="User Avatar" />
+      <UserSpaceContentProvider>
+        <div className="background-image"></div>
+        <MyTravelogHeader />
+        <div className="user-space">
+
+          <div className="content">
+            <div className="avatar">
+              <img src={avatarUrl} alt="User Avatar" />
+            </div>
+
+            <div className="user-details">
+              <div className="username">{userName ? userName : "MOMO"}</div>
+              <div className="uid">UID: {UID ? UID : 1145141919810}</div>
+            </div>
+          </div>
         </div>
-        <div className="user-details">
-          <div className="username">{userName?userName:"MOMO"}</div>
-          <div className="uid">UID: {UID?UID:1145141919810}</div>
-        </div>
-      </div>
-    </div>
-      <MyTravelogFilter />
-      <MyTravelogList myTravelogList={myTravelogList} />
-      {totop ? (
-        <div
-          className={styles.totop}
-          onClick={() =>  {
-            // 点击后滚动到页面顶部
-            window.scrollTo({
-              top: 0,
-              behavior: 'smooth' // 平滑滚动
-            });}}>
-          <UpOutline style={{ fontSize: "1rem" }} />
-        </div>
-      ) : (
-        <></>
-      )}
+        
+        <MyTravelogFilter />
+        <MyTravelogList myTravelogList={myTravelogList} />
+        {totop ? (
+          <div
+            className={styles.totop}
+            onClick={() => {
+              // 点击后滚动到页面顶部
+              window.scrollTo({
+                top: 0,
+                behavior: 'smooth' // 平滑滚动
+              });
+            }}>
+            <UpOutline style={{ fontSize: "1rem" }} />
+          </div>
+        ) : (
+          <></>
+        )}
       </UserSpaceContentProvider>
     </>
   )
