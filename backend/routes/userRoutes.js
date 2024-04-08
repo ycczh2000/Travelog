2e988df244541d19f6eccd71460a8719ccd9b079
Sueyuki <2574397962@qq.com>
2024-04-08 22:22:33 +0800

    优化了头像上传组件的显示和验证

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: Sueyuki 2574397962@qq.com
+ * @Date: 2024-04-02 19:17:09
+ * @LastEditors: Sueyuki 2574397962@qq.com
+ * @LastEditTime: 2024-04-08 21:59:48
+ * @FilePath: \backend\routes\userRoutes.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 const express = require("express")
 const router = express.Router()
 const User = require("../models/User")
@@ -40,9 +48,9 @@ router.post("/uploadAvator", avatarImgUpload.single("image"), async (req, res) =
 //将用户头像返回给前端
 router.get("/getAvatar/:username", async (req, res) => {
   const username = req.params.username
-  console.log('将用户头像返回给前端',username)
-  // const result = await User.getAvatar(username)
-  console.log(result)
+  // console.log('将用户头像返回给前端',username)
+  const result = await User.getAvatar(username)
+  // console.log(result)
   if (result.success) {
     const { avatar, type } = result.data
     res.type(type)
