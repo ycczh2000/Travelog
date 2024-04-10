d0c808330c20440c34738c29861a7dfdc34832e7
Sueyuki <2574397962@qq.com>
2024-04-10 15:07:03 +0800

    修复了选择搜索模式时弹出了一个额外的框的bug

--- frontend/src/ConditionalFooter.js
+++ frontend/src/ConditionalFooter.js
@@ -14,10 +14,9 @@ function ConditionalFooter() {
   const location = useLocation();
   const hideFooterOnPaths = ["/", "/register", "/publish", "/login", "/publish/", "/travelogs"];
 
-  // 添加正则表达式以确保形如 /update/66114c55bfe46e3f74f02646 的路径也不显示 Footer
   const shouldHideFooter = hideFooterOnPaths.includes(location.pathname) || 
                           location.pathname.match(/^\/travelogs\/[^/]+$/) ||
-                          location.pathname.match(/^\/update\/[a-f\d]{24}$/); // 匹配形如 /update/66114c55bfe46e3f74f02646
+                          location.pathname.match(/^\/update\/[a-f\d]{24}$/);
 
   return shouldHideFooter ? null : <Footer />;
 }
--- frontend/src/components/SearchBar/SearchBar.js
+++ frontend/src/components/SearchBar/SearchBar.js
@@ -2,7 +2,7 @@
  * @Author: Sueyuki 2574397962@qq.com
  * @Date: 2024-04-02 19:17:09
  * @LastEditors: Sueyuki 2574397962@qq.com
- * @LastEditTime: 2024-04-10 14:10:05
+ * @LastEditTime: 2024-04-10 15:06:11
  * @FilePath: \frontend\src\components\SearchBar\SearchBar.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -196,7 +196,7 @@ const SearchBarComponent = () => {
             </div>
             <Popup
               closeOnSwipe='true'
-              visible={visible}
+              visible={seacrchPageVisible}
               onMaskClick={() => {
                 setSeacrchPageVisible(false)
               }}
