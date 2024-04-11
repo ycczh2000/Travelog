d8960e3127ae128f18d1cefe012ee64a85532b63
Sueyuki <2574397962@qq.com>
2024-04-11 18:22:13 +0800

    给登录页增加了异常处理，修复了footer刷新页面后选项不正确的错误

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -2,20 +2,25 @@
  * @Author: Sueyuki 2574397962@qq.com
  * @Date: 2024-04-02 19:17:09
  * @LastEditors: Sueyuki 2574397962@qq.com
- * @LastEditTime: 2024-04-10 13:34:27
+ * @LastEditTime: 2024-04-11 18:16:07
  * @FilePath: \frontend\src\components\Footer\Footer.jsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FaHome, FaHeart, FaRegPlusSquare, FaEnvelope, FaUser } from 'react-icons/fa';
 import "./footer.css";
 export default function Footer() {
-  const [selected, setSelected] = useState(""); // 默认选择首页
+  const initialSelected = localStorage.getItem("selected") || "home";
+  const [selected, setSelected] = useState(initialSelected);
 
   const handleSelect = (item) => {
     setSelected(item);
   };
+
+  useEffect(() => {
+    localStorage.setItem("selected", selected);
+  }, [selected]);
   return (
 <div className="footer">
   <Link to="/home" onClick={() => handleSelect("home")} className={selected === "home" ? "selected" : ""}>
