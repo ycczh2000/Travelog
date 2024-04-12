3a5a9b8aaebfbb25cc4e434a53d6c7a2907a75b6
ycczh2000 <ycczh200@163.com>
2024-04-12 08:46:54 +0800

    移除不需要的userspace相关组件，将请求默认地址设为localhost，如果要访问云服务器，请自行修改src/config文件

--- frontend-admin/src/config/config.js
+++ frontend-admin/src/config/config.js
@@ -1,4 +1,4 @@
-export const baseURL = "http://47.102.221.158:8000/"
-// export const baseURL = "http://localhost:8000/"
+// export const baseURL = "http://47.102.221.158:8000/"
+export const baseURL = "http://localhost:8000/"
 
 //选择远程或本地服务器
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,12 @@ import { Routes, Route } from "react-router"
 import { BrowserRouter } from "react-router-dom"
 import Home from "./pages/Home/Home"
 import MyTravelog from "./pages/MyTravelog/MyTravelog"
-
 import Login from "./pages/Login/Login"
 import Register from "./pages/Register/Register"
 import ConditionalFooter from "./ConditionalFooter"
-import UserSpace from "./pages/UserSpace/UserSpace"
-import UserTravelogs from "./pages/UserTravelogs/UserTravelogs"
 import Publish from "./pages/Publish/Publish"
 import Detaillog from "./pages/Detaillog/Detaillog"
 import PreviewPage from "./pages/PreviewPage/PreviewPage"
-
 import { UserContextProvider } from "./Context/UserContext"
 
 function App() {
@@ -23,9 +19,6 @@ function App() {
           <Routes>
             <Route path="/home" element={<Home />}></Route>
             <Route path="/travelogs/:id" element={<Detaillog />}></Route>
-            <Route path="/space/:username" element={<UserSpace />}>
-              <Route path="travelog" element={<UserTravelogs />}></Route>
-            </Route>
             <Route path="/mytravelog" element={<MyTravelog />}></Route>
             <Route path="/publish" element={<Publish />}></Route>
             <Route path="/update/:targetTravelogId" element={<Publish />}></Route>
